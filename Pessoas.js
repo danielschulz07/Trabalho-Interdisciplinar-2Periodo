341005aa9bb846a8064d06beb244f33cf0c7bdbd
rNtop4444 <gaminganddeving@gmail.com>
2025-11-26 13:39:17 +0000

    criao objeto pessoas

---
+++ Pessoas.js
@@ -0,0 +1,43 @@
+export class Pessoa {
+    static #idPessoa = 0;
+    #nome;
+    #cpf;
+    #dtNascimento;
+    constructor (nome, cpf, dtNasc) {
+        this.#nome = nome;
+        this.#cpf = cpf;
+        this.#dtNascimento = dtNasc;
+
+        Pessoa.#idPessoa++;
+    }
+    /*... as implementações dos métodos get e set seguem
+    os códigos mostrados anteriormente para a classe Cliente
+    */
+
+    get nome(){
+        return this.#nome;
+    }
+    set nome(nome){
+        if (nome.length != 0){
+            this.#nome = nome;
+        }
+    }
+
+    get cpf(){
+        return this.#cpf;
+    }
+
+    get dtNascimento(){
+        return this.#dtNascimento;
+    }
+
+    set dtNascimento(dtNascimento){
+        this.#dtNascimento = dtNascimento;
+    }
+
+    toString() {
+        return ("\nNome: " + this.#nome +
+        "\nCPF: " + this.#cpf +
+        "\nNascimento: " + this.#dtNascimento);
+    } 
+}
